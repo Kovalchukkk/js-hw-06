6031726fa5a3c0b6f67dbd8eb3187fa00d5f42f7
Mykola Kovalchuk <kolyankov@gmail.com>
2022-07-08 21:44:42 +0300

    add task-2

--- js/task-2.js
+++ js/task-2.js
@@ -1,1 +1,23 @@
+const ingredients = [
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
+];
 
+const createList = (options) => {
+  return options.map((ingredient) => {
+    const itemRef = document.createElement("li");
+    itemRef.textContent = ingredient;
+    itemRef.classList.add("item");
+    return itemRef;
+  });
+};
+
+const ingredientsRef = document.querySelector("#ingredients");
+
+const itemListRef = createList(ingredients);
+
+ingredientsRef.append(...itemListRef);
