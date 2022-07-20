5c2a4e83dfa344d28fa4d4a18f44ab98d60c4286
Mykola Kovalchuk <kolyankov@gmail.com>
2022-07-20 21:15:52 +0300

    update task-10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,13 @@
+const inputRef = document.querySelector("input");
+const btnCreate = document.querySelector("button[data-create]");
+const btnDestroy = document.querySelector("button[data-destroy]");
+
+function createBoxes(amount) {
+  for (let i = 0; i < amount; i += 1) {
+    const divRef = document.createElement("div");
+  }
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
